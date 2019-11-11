90d7c7a140f684f768b04e9c2e5d793cfb8a131b
WylieYYYY <wylieyyyy@gmail.com>
2019-11-12 05:19:40 +0800

    Compatibility to C++

--- shellfront.h
+++ shellfront.h
@@ -1,6 +1,10 @@
 #ifndef SHELLFRONT_H
 #define SHELLFRONT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
 // configuration structure
 struct term_conf {
 	// window gravity toward edges
@@ -29,5 +33,9 @@ int shellfront_interpret(int argc, char **argv);
 void shellfront_catch_io_from_arg(int argc, char **argv);
 // you decide how the application is presented to the user
 void shellfront_catch_io(int argc, char **argv, struct term_conf config);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
